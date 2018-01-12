21b221836d30b7a391b8b7ca50e6747dd7630c24
vivekanand-mathapati <vivekanand.d.mathapati@gmail.com>
2018-01-12 15:33:53 +0530

    unitesting of arithmatic operations

---
+++ arithmatic.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    return x + y
+
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide():
+    if y == 0:
+        raise ValueError
+    else:
+        return x/y
+
